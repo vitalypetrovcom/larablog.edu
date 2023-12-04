5e6a7e9cdb6b7edd22462be2ad75ac7255120c35
Vitaly <vitaly.petrov.com@gmail.com>
2023-12-04 17:38:32 +0300

    Урок 2.21. Данные сайдбара

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -24,6 +27,36 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        Schema::defaultStringLength(191); // Запись для решения проблемы с ошибкой при выполнении миграции
+
+
+        view ()->composer ('layouts.sidebar', function ($view) { // Сайдбар, Popular Posts и Categories. Использование метода View Composer для страниц сайта. Аргументом мы указываем для какого вида мы будем передавать эти данные. Аргументом в коллбекфункцию передаем $view (в нее мы будем передавать наши данные)
+
+        // Кеширование сайдбара
+        // Запрашиваем из кеша данные
+        if (Cache::has('cats')) { // Обращаемся к классу Facade\Cache, проверяем методом has есть ли у нас в кэше данные по ключу 'cats' (для категорий). Если есть, тогда в переменную $cats мы возьмем данные из кэша
+            $cats = Cache::get ('cats');
+        } else { // Если их там нет, тогда нам нужно их получить
+            $cats = Category::withCount('posts')->orderBy ('posts_count', 'desc')->get ();
+        // Кладем в кэш по ключу 'cats' данные переменной $cats на время 30сек
+        Cache::put ('cats', $cats, 30);
+    }
+
+        // Получаем популярные посты Popular Posts
+            $view->with('popular_posts', Post::orderBy('views', 'desc')->limit (3)->get()); // Стараемся дать переменной уникальное название, чтобы не было совпадений с другими переменными. Для получения постов нам нужно обратиться к модели Post, нам нужно выбирать посты по признаку views (количество просмотров), отсортируем посты в обратном порядке, получаем записи с помощью метода get()
+
+        // Получаем категории Categories
+        /*$view->with('cats', Category::withCount('posts')->orderBy ('posts_count', 'desc')->get ());*/
+            $view->with('cats', $cats); // Передаем в представление $cats либо из кэша, либо мы их получим из БД
+
+        // Для получения категорий нам нужно обратиться к модели Category, нам нужно выбирать категории по признаку views (количество постов), отсортируем категории в обратном порядке, получаем записи с помощью метода get().
+            // Как нам получить категории и через связи еще получить сколько постов находится в этих категориях? Здесь мы будем использовать метод withCount (https://laravel.com/docs/8.x/eloquent-relationships#counting-related-models) с названием связи, которая нас интересует 'posts' - в этом случае, у нас будет доступно свойство формата {posts}_count. В этом свойстве у нас будет храниться количество постов для конкретной категории.
+
+        } );
+
+
+
+
+
     }
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,46 +2,36 @@
 
 <div class="sidebar">
         <div class="widget">
-            <h2 class="widget-title">Recent Posts</h2>
+            <h2 class="widget-title">Popular Posts</h2>
             <div class="blog-list-widget">
+                @foreach($popular_posts as $post)
+
                 <div class="list-group">
                     <a href="{{ route('posts.single', ['slug' => $post->slug]) }}" class="list-group-item list-group-item-action flex-column align-items-start">
                         <div class="w-100 justify-content-between">
-                            <img src="/assets/front/upload/small_07.jpg" alt="" class="img-fluid float-left">
-                            <h5 class="mb-1">5 Beautiful buildings you need to before dying</h5>
-                            <small>12 Jan, 2016</small>
+                            <img src="{{ $post->getImage () }}" alt="" class="img-fluid float-left">
+                            <h5 class="mb-1">{{ $post->title }}</h5>
+                            <small>{{ $post->getPostDate () }}</small>
+                            <small>| <i class="fa fa-eye"></i> {{ $post->views }}</small>
                         </div>
-                    </a>
-
-                    <a href="{{ route('posts.single', ['slug' => $post->slug]) }}" class="list-group-item list-group-item-action flex-column align-items-start">
-                        <div class="w-100 justify-content-between">
-                            <img src="/assets/front/upload/small_08.jpg" alt="" class="img-fluid float-left">
-                            <h5 class="mb-1">Let's make an introduction for creative life</h5>
-                            <small>11 Jan, 2016</small>
-                        </div>
-                    </a>
-
-                    <a href="{{ route('posts.single', ['slug' => $post->slug]) }}" class="list-group-item list-group-item-action flex-column align-items-start">
-                        <div class="w-100 last-item justify-content-between">
-                            <img src="/assets/front/upload/small_09.jpg" alt="" class="img-fluid float-left">
-                            <h5 class="mb-1">Did you see the most beautiful sea in the world?</h5>
-                            <small>07 Jan, 2016</small>
-                        </div>
-                    </a>
+                        </a>
                 </div>
+
+                @endforeach
+
             </div><!-- end blog-list -->
         </div><!-- end widget -->
 
-        <div id="" class="widget">
+        {{--<div id="" class="widget">
             <h2 class="widget-title">Advertising</h2>
             <div class="banner-spot clearfix">
                 <div class="banner-img">
                     <img src="/assets/front/upload/banner_03.jpg" alt="" class="img-fluid">
                 </div><!-- end banner-img -->
             </div><!-- end banner -->
-        </div><!-- end widget -->
+        </div><!-- end widget -->--}}
 
-        <div class="widget">
+        {{--<div class="widget">
             <h2 class="widget-title">Instagram Feed</h2>
             <div class="instagram-wrapper clearfix">
                 <a class="" href="#"><img src="/assets/front/upload/small_09.jpg" alt="" class="img-fluid"></a>
@@ -54,19 +44,15 @@
                 <a href="#"><img src="/assets/front/upload/small_07.jpg" alt="" class="img-fluid"></a>
                 <a href="#"><img src="/assets/front/upload/small_08.jpg" alt="" class="img-fluid"></a>
             </div><!-- end Instagram wrapper -->
-        </div><!-- end widget -->
+        </div><!-- end widget -->--}}
 
         <div class="widget">
-            <h2 class="widget-title">Popular Categories</h2>
+            <h2 class="widget-title">Categories</h2>
             <div class="link-widget">
                 <ul>
-                    <li><a href="#">Marketing <span>(21)</span></a></li>
-                    <li><a href="#">SEO Service <span>(15)</span></a></li>
-                    <li><a href="#">Digital Agency <span>(31)</span></a></li>
-                    <li><a href="#">Make Money <span>(22)</span></a></li>
-                    <li><a href="#">Blogging <span>(66)</span></a></li>
-                    <li><a href="#">Entertaintment <span>(11)</span></a></li>
-                    <li><a href="#">Video Tuts <span>(87)</span></a></li>
+                @foreach($cats as $cat)
+                    <li><a href="{{ route ('categories.single', ['slug' => $cat->slug]) }}">{{ $cat->title }}<span>({{ $cat->posts_count }})</span></a></li>
+                @endforeach
                 </ul>
             </div><!-- end link-widget -->
         </div><!-- end widget -->
